946c28e832433a8944a54317fb0d49c7ebfea1b1
SebastianPinto2424 <sebastian.pinto2301@alumnos.ubiobio.cl>
2026-04-26 17:49:55 -0400

    Se agrego una funcion de multiplicar

--- funciones.c
+++ funciones.c
@@ -12,6 +12,10 @@ int resta(int a, int b) {
     return a - b;
 }
 
+int resta(int a, int b) {
+    return a * b;
+}
+
 int main() {
 
     int num1=2, num2=3;
@@ -20,6 +24,8 @@ int main() {
     printf("Suma: %d + %d = %d\n", num1, num2, sumar(num1, num2));
     
     printf("Resta: %d - %d = %d\n", num1, num2, resta(num1, num2));
+
+    printf("Multiplicación: %d * %d = %d\n", num1, num2, multiplicacion(num1, num2));
 
     return 0;
 }
